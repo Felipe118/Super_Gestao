8408a3912c25305d30ee8fc22b30aa53a757dd6f
Felipe118 <lipebezerraa@gmail.com>
2021-07-14 16:01:50 -0300

    Criando o pagina produtos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Produto;
+use App\Unidade;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
@@ -26,8 +27,10 @@ class ProdutoController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
+
     {
-        //
+        $unidades = Unidade::all();
+        return view('app.produto.create',['unidades' => $unidades]);
     }
 
     /**
